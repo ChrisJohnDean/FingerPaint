538ec3334147c423b07fca1743771d7af1a49c4a
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-23 17:19:27 -0800

    Initial Commit

---
+++ FingerPaint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FingerPaint
+//
+//  Created by Chris Dean on 2018-02-23.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
